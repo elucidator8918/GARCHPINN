8ad4abaddbfe32f60d1fea510d1dca84a5e03a04
Siddhant <forsomethingnewsid@gmail.com>
2025-04-14 04:10:55 +0530

    Update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -4,19 +4,38 @@ import yfinance as yf
 import torch
 from torch.utils.data import TensorDataset, DataLoader
 
-def get_data(start_date='2015-01-01', end_date='2022-12-31'):
+def get_data(start_date='2015-01-01', end_date='2022-12-31', dataset="FTSC"):
     """Download S&P 500 data and calculate log returns and realized variance"""
-    print("Downloading S&P 500 data...")
-    sp500 = yf.download('^GSPC', start=start_date, end=end_date)
+    if dataset =="FTSC":
+        df = pd.read_csv("UK_Index_2000_2025.csv")
+        df['Date-Time'] = pd.to_datetime(df['Date-Time'])
+        df = df.dropna(axis=1, how='all')
+        
+        df['Date-Time'] = df.apply(
+            lambda row: row['Date-Time'] + pd.Timedelta(hours=row['GMT Offset']),
+            axis=1
+        )
+        
+        df = df.drop(columns=["#RIC", "Domain", "Type", "No. Trades", "GMT Offset"])
 
-    # Calculate log returns
-    sp500['log_return'] = np.log(sp500['Close'] / sp500['Close'].shift(1))
-    sp500['realized_var'] = sp500['log_return'].rolling(window=5).var() * 252  # Annualized
+        # Calculate log returns
+        df['log_return'] = np.log(df['Last'] / df['Last'].shift(1))
+        df['realized_var'] = df['log_return'].rolling(window=10).var() * 19656
 
-    # Drop NA values
-    sp500 = sp500.dropna()
+        return df
+    
+    else:
+        print("Downloading S&P 500 data...")
+        sp500 = yf.download('^GSPC', start=start_date, end=end_date)
+    
+        # Calculate log returns
+        sp500['log_return'] = np.log(sp500['Close'] / sp500['Close'].shift(1))
+        sp500['realized_var'] = sp500['log_return'].rolling(window=5).var() * 252  # Annualized
+    
+        # Drop NA values
+        sp500 = sp500.dropna()
 
-    return sp500
+        return sp500
 
 def prepare_data(df, sequence_length=20, device='cpu'):
     """Prepare data for PINN training"""
